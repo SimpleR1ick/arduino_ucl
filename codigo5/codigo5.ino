39b13f40e4b94e4f054384fc23c19ba830251105
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2023-04-13 21:45:35 -0300

    Correção codigo

--- codigo5/codigo5.ino
+++ codigo5/codigo5.ino
@@ -27,38 +27,38 @@ void loop() {
         count++;
       }
     }
-  }
-  // ENTRADA DE DADOS
-  Serial.println("Valores recebidos:");
-  Serial.print(mensagem[0]);
-  Serial.print(mensagem[1]);
-  Serial.print(mensagem[2]);
+    // ENTRADA DE DADOS
+    Serial.println("Valores recebidos:");
+    Serial.print(mensagem[0]);
+    Serial.print(mensagem[1]);
+    Serial.print(mensagem[2]);
 
-  // ZERA O CONTADOR
-  count = 0;
+    // ZERA O CONTADOR
+    count = 0;
 
-  // LIGA O LED AZUL
-  if (mensagem[0] == '1') {
-    digitalWrite(led_blu, HIGH);
-  }
-  // LIGA O LED VERDE
-  if (mensagem[1] == '1') {
-    digitalWrite(led_gre, HIGH);
-  }
-  // LIGA O VERMELHO
-  if (mensagem[2] == '1') {
-    digitalWrite(led_red, HIGH);
-  }
-  // DESLIGA O LED AZUL
-  if (mensagem[0] == '0') {
-    digitalWrite(led_blu, LOW);
-  }
-  // DESLIGA O LED VERDE
-  if (mensagem[1] == '0') {
-    digitalWrite(led_gre, LOW);
-  }
-  // DESLIGA O LED VERMELHO
-  if (mensagem[2] == '0') {
-    digitalWrite(led_red, LOW);
+    // LIGA O LED AZUL
+    if (mensagem[0] == '1') {
+      digitalWrite(led_blu, HIGH);
+    }
+    // LIGA O LED VERDE
+    if (mensagem[1] == '1') {
+      digitalWrite(led_gre, HIGH);
+    }
+    // LIGA O VERMELHO
+    if (mensagem[2] == '1') {
+      digitalWrite(led_red, HIGH);
+    }
+    // DESLIGA O LED AZUL
+    if (mensagem[0] == '0') {
+      digitalWrite(led_blu, LOW);
+    }
+    // DESLIGA O LED VERDE
+    if (mensagem[1] == '0') {
+      digitalWrite(led_gre, LOW);
+    }
+    // DESLIGA O LED VERMELHO
+    if (mensagem[2] == '0') {
+      digitalWrite(led_red, LOW);
+    }
   }
 }
